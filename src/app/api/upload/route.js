050215c48bffe6dc578a964cbfe7eec7dcf3adfc
LostoVayne <moises.barrera.dev@gmail.com>
2023-08-29 00:50:16 -0400

    finish

---
+++ src/app/api/upload/route.js
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+import path from "path";
+import { writeFileSync } from "fs";
+
+cloudinary.config({
+    cloud_name: "dndyttpfh",
+    api_key: "249964259886213",
+    api_secret: "_KNVBynQqirRNQXtujlQtF-Q-wE",
+});
+
+export async function POST(request) {
+    const data = await request.formData();
+    const image = data.get("image");
+
+    if (!image.name) {
+        return NextResponse.json("no se ha subido ninguna imagen");
+    }
+
+    const bytes = await image.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    //guardar en un archivo en nuestro pc
+    // const filepath = path.join(process.cwd(), "public", image.name);
+    // writeFileSync(filepath, buffer);
+
+    const response = await new Promise((resolve, reject) => {
+        cloudinary.uploader
+            .upload_stream({}, async (error, response) => {
+                if (error) {
+                    return reject(error);
+                }
+
+                resolve(response);
+            })
+            .end(buffer);
+    });
+
+    return NextResponse.json({
+        url: response.secure_url,
+    });
+}
